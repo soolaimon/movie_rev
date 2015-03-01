df0a90500375a0bb771b4b8d67579f76319c8946
Dave Nelson <davidicus21@gmail.com>
2015-03-01 10:16:14 -0700

    adds model & controller for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,11 @@
 class Review < ActiveRecord::Base
+  before_create :set_rt_data
+
+
+
+  private
+
+  def set_rt_data
+    
+  end
 end
--- db/migrate/20150301035546_create_reviews.rb
+++ db/migrate/20150301035546_create_reviews.rb
@@ -1,9 +1,11 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.string :title
-      t.string :body
-      t.string :rt_id
+      t.string :title, null: false
+      t.string :body, null: false
+      t.integer :rt_id, default: 0
+      t.string :rt_img_url, default: "http://d3biamo577v4eu.cloudfront.net/static/images/redesign/poster_default.gif"
+      t.boolean :fresh
 
       t.timestamps null: false
     end

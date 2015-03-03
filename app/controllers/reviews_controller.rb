99eef990aae04030cea74d05185a69b310edddc2
Dave Nelson <davidicus21@gmail.com>
2015-03-02 18:56:45 -0700

    adds review creation functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,10 +8,28 @@ class ReviewsController < ApplicationController
   def show
   end
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def review_params
+    params.require(:review).permit(:id, :title, :body, :fresh)
+  end
 
   def get_review
     @review = Review.find(params[:id])
   end
+
 
 end
